a6287a574028fe1a970e04260d38a2e400a168f9
adam6806 <asmith0935@gmail.com>
2018-08-18 17:18:08 -0400

    initial commit

---
+++ src/main/java/com/github/adam6806/pnlanalyzer/PnlanalyzerApplication.java
@@ -0,0 +1,12 @@
+package com.github.adam6806.pnlanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnlanalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PnlanalyzerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/adam6806/pnlanalyzer/PnlanalyzerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.adam6806.pnlanalyzer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PnlanalyzerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
