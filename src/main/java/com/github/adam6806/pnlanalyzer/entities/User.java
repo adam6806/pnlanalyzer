15f56a9ea4b3adbae20ee8dbdcfc4f56cee852bf
adam6806 <asmith0935@gmail.com>
2018-09-01 16:56:07 -0400

    PNLAN-57 Remove registration from login page #time 8h 17m #comment ADDED COOL COLORED BADGES! I also fixed the cascade problem

--- src/main/java/com/github/adam6806/pnlanalyzer/entities/User.java
+++ src/main/java/com/github/adam6806/pnlanalyzer/entities/User.java
@@ -37,7 +37,7 @@ public class User {
     @Column(name = "active")
     private int active;
 
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
